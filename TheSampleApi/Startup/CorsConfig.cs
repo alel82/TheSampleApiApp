d7d1cab2ee8496f06a7e24c4956e261ab86db13f
Azrel <azril.izza@gmail.com>
2026-07-18 19:34:44 +0800

    Added a UI (Blazor WebAssembly StandAlone)

---
+++ TheSampleApi/Startup/CorsConfig.cs
@@ -0,0 +1,24 @@
+﻿namespace TheSampleApi.Startup;
+
+public static class CorsConfig
+{
+    private const string _allowAllPolicy = "AllowAll";
+
+    public static void AddCorsServices(this IServiceCollection services)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy(_allowAllPolicy, policy =>
+            {
+                policy.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+            });
+        });
+    }
+
+    public static void ApplyCorsConfig(this WebApplication app)
+    {
+        app.UseCors(_allowAllPolicy);
+    }
+}
